09a1f0cd2d6b890f28eff2d9875f42281d05868c
yqpmateo <58517098+yqpmateo@users.noreply.github.com>
2019-12-06 10:46:49 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>My First JavaScript</h2>
+<button type="button"
+onclick="document.getElementById('demo').innerHTML = Date()">
+Click me to display Date and Time.</button>
+<p id="demo"></p>
+<hr size="2" noshade>
+<?php
+    phpinfo();
+?>
+
+</body>
+</html> 
